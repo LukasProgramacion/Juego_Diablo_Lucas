6cc67b5d3df150de43efe6c852b553e38ef24464
LukasProgramacion <lukas.arts315@gmail.com>
2024-11-28 21:22:44 +0100

    Avance Juego Diablo (28/11/24)

---
+++ Assets/Scripts/Cofre.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cofre : MonoBehaviour
+{
+    private Outline outline;
+
+    [SerializeField] private Texture2D cursorInteraccion;
+    [SerializeField] private Texture2D cursorPorDefecto;
+
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorInteraccion, Vector2.zero, CursorMode.Auto);
+        outline.enabled = true;
+        Debug.Log("hE entrao");
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorPorDefecto, Vector2.zero, CursorMode.Auto);
+        outline.enabled = false;
+        Debug.Log("He salido");
+    }
+}
