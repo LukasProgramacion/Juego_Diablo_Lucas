c766b232d0b9900f91db05131481b45353839872
LukasProgramacion <lukas.arts315@gmail.com>
2024-12-04 21:20:41 +0100

    Avance Juego Diablo (04/12/24)

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,18 +1,34 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    private Outline outline;
+
+    [SerializeField] private float tiempoRotacion;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        outline = GetComponent<Outline>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Interactuar (Transform interactuador)
     {
-        
+        Debug.Log("Hola");
+        transform.DOLookAt(interactuador.position, tiempoRotacion, AxisConstraint.Y);
+    }
+
+    private void OnMouseEnter()
+    {
+        outline.enabled = true;
+
+    }
+
+    private void OnMouseExit()
+    {
+        outline.enabled = false;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float distanciaInteraccion;
+    
     private Camera cam;
     
     private NavMeshAgent agent;
+
+    private NPC npcActual;
 
 
     // Start is called before the first frame update
@@ -21,11 +26,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out RaycastHit hit))
+        Movimiento();
+
+        if(npcActual)
         {
-            if(Input.GetMouseButtonDown(0))
+            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
             {
+                npcActual.Interactuar(this.transform);
+                npcActual = null;
+                agent.isStopped = true;
+                agent.stoppingDistance = 0;
+            }
+        }
+        
+        
+    }
+
+    private void Movimiento()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Mirar a ver si el punto donde he impactado tiene el script "NPC"
+                if (hit.transform.TryGetComponent(out NPC npc))
+                {
+                    //y en ese caso, ese npc es el actual.
+                    npcActual = npc;
+                    //ahora mi distancia de parada es la de inerccion (a x metros del npc)
+                    agent.stoppingDistance = distanciaInteraccion;
+                }
+
                 agent.SetDestination(hit.point);
             }
         }
