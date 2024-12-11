11fd91e39a2542a2752dac41aa7a42c7893fb337
LukasProgramacion <lukas.arts315@gmail.com>
2024-12-11 21:22:28 +0100

    Avance Juego Diablo (11/12/24)

---
+++ Assets/Scripts/PlayerVisual.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerVisual : MonoBehaviour
+{
+    private Animator anim;
+
+    [SerializeField] private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("velocity", agent.velocity.magnitude / agent.speed);
+    }
+}
